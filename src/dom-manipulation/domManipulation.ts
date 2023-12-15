61745e745799feb735f176e3627d3777010cf4a0
leireF <leire.olabarri@opendeusto.es>
2023-12-15 20:19:10 +0100

    Cambios dom-manipulation

--- src/dom-manipulation/domManipulation.ts
+++ src/dom-manipulation/domManipulation.ts
@@ -2,16 +2,39 @@ import { DayOfWeek, WeatherIcon, WeatherIcontype, WeatherResponse } from "../mod
 
 // TODO: Create references for all the html elements
 export const buttonClick = document.getElementById("button-location");
+const temperature =  document.getElementById("weather-temp");
+const weatherDescription = document.getElementById("weather-desc");
 const WeatherIconPng = document.getElementById("weather-icon");
-
+const LocationText = document.getElementById("location-text");
+const DateDayName = document.getElementById("date-dayname");
+const DateDay = document.getElementById("date-day");
+const maxTemp = document.getElementById("text-temp-max");
+const minTemp = document.getElementById("text-temp-min");
+const humidity = document.getElementById("text-humidity");
+const wind = document.getElementById("text-wind");
+const locationInput = document.getElementById("weather-location-input");
 
 // TODO: Create the logic of the function
-export const updateInteface = (weather: WeatherResponse) :void => {
-    
-}
+export const updateInteface = (weather: WeatherResponse): void => {
+    if (temperature) temperature.textContent = Math.floor(weather.main.temp).toString() + "ºC";
+    if (weatherDescription) weatherDescription.textContent = weather.weather[0].main;
+    changeWeatherIcon(weather.weather[0].icon ?? '01d');
 
-// TODO: Get the city from the input element
+    if (LocationText) LocationText.textContent = weather.name;
+    if (DateDayName) DateDayName.textContent = getDayOfWeek();
+    if (DateDay) DateDay.textContent = getDate();
+
+    if (maxTemp) maxTemp.textContent = Math.floor(weather.main.temp_max) + " ºC";
+    if (minTemp) minTemp.textContent = Math.floor(weather.main.temp_min) + " ºC";
+    if (humidity) humidity.textContent = weather.main.humidity.toString() + " %";
+    if (wind) wind.textContent = weather.wind.speed.toString() + " m/s";
+};
+
+// Helper function to get the day of the week
 export function getCity(): string {
+    if(locationInput) {
+        return (locationInput as HTMLInputElement).value;
+    }
     return "";
 }
 
